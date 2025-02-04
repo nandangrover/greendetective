f2ba97dd39dc9bb7d02e935764b2da3b3feb4cd5
Nandan Grover <nandangrover.5@gmail.com>
2025-02-04 22:13:49 +0000

    made everything cheaper

--- main.tf
+++ main.tf
@@ -11,8 +11,9 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-2a"  # Explicitly set AZ
   tags = {
     Name = "green-detective-public-subnet"
   }
